471d0aa0274921a52b8adc43d58a4b565560eb10
reyrizki <reynaldirizki97.rr@gmail.com>
2021-10-27 20:31:03 +0700

    feature(Bootstrap): Configure Bootstrap Plugin
    - Change to Vue2
    - Add Bootstrap Plugin
    - Add Basic Navbar

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,29 +1,23 @@
 <template>
-  <div>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div id="app">
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <div class="container">
+        <b-navbar-brand> Actors </b-navbar-brand>
+      </div>
+    </b-navbar>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
+import { Component, Vue } from "vue-property-decorator";
+import { BNavbar } from "bootstrap-vue";
 
-@Options({
+@Component({
   components: {
-    HelloWorld,
+    BNavbar,
   },
 })
 export default class App extends Vue {}
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"></style>
