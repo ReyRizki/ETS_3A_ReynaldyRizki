16a2bb6aeb3e3b00e538937512c8c6f2f0c73969
reyrizki <reynaldirizki97.rr@gmail.com>
2021-10-28 11:41:37 +0700

    feature(Front-End): Add and Update Actor
    - Create actor input modal
    - Create add and update function

---
+++ front-end/src/components/ActorModal.vue
@@ -0,0 +1,53 @@
+<template>
+  <b-modal v-model="showModal" size="lg" centered static>
+    <template #modal-header="">
+      <h4>{{ title }}</h4>
+      <b-button size="sm" variant="outline-dark" @click="hideModal()"> X </b-button>
+    </template>
+    <template>
+      <b-input-group prepend="Name" class="mb-2">
+        <b-form-input aria-label="First name" v-model="firstName"></b-form-input>
+        <b-form-input aria-label="Last name" v-model="lastName"></b-form-input>
+      </b-input-group>
+    </template>
+    <template #modal-footer="">
+      <b-button size="sm" variant="outline-dark" @click="hideModal()"> Cancel </b-button>
+      <b-button size="sm" variant="dark" @click="okAction()"> OK </b-button>
+    </template>
+  </b-modal>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
+import { BModal, BButton, BInputGroup, BFormInput } from "bootstrap-vue";
+
+@Component({
+  components: { BModal, BButton, BInputGroup, BFormInput },
+})
+export default class ActorModal extends Vue {
+  @Prop({ required: true, default: true }) showModal!: boolean;
+  @Prop({ required: true }) readonly title!: string;
+  @Prop() readonly actor!: any;
+
+  private firstName = "";
+  private lastName = "";
+
+  hideModal(): void {
+    this.$emit("toggle-modal");
+  }
+
+  okAction(): void {
+    this.$emit("action", this.firstName, this.lastName);
+
+    this.hideModal();
+  }
+
+  @Watch("actor")
+  actorChanged(newVal: any) {
+    if (this.actor) {
+      this.firstName = newVal.first_name;
+      this.lastName = newVal.last_name;
+    }
+  }
+}
+</script>
--- front-end/src/components/ActorTable.vue
+++ front-end/src/components/ActorTable.vue
@@ -1,8 +1,21 @@
 <template>
   <div id="table">
+    <actor-modal
+      title="Add Actor"
+      :showModal="showAddModal"
+      @toggle-modal="toggleModal('add')"
+      @action="addActor"
+    />
+    <actor-modal
+      title="Update Actor"
+      :showModal="showUpdateModal"
+      :actor="currentActor"
+      @action="updateActor"
+      @toggle-modal="toggleModal('update')"
+    />
     <div class="container mt-3">
       <div class="d-flex justify-content-end">
-        <b-button variant="dark"> Add Actor </b-button>
+        <b-button variant="dark" @click="toggleModal('add')"> Add Actor </b-button>
       </div>
       <b-table
         id="actor-table"
@@ -18,7 +31,13 @@
         </template>
         <template #cell(actions)="row">
           <div class="d-flex gap-1">
-            <b-button size="sm" variant="outline-dark"> Update </b-button>
+            <b-button
+              size="sm"
+              variant="outline-dark"
+              @click="openUpdateModal(row.index)"
+            >
+              Update
+            </b-button>
             <b-button size="sm" @click="deleteActor(row.index)" variant="dark">
               Delete
             </b-button>
@@ -41,12 +60,14 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import { BNavbar, BTable, BButton } from "bootstrap-vue";
+import ActorModal from "./ActorModal.vue";
 
 @Component({
   components: {
     BNavbar,
     BTable,
     BButton,
+    ActorModal,
   },
 })
 export default class ActorTable extends Vue {
@@ -57,9 +78,51 @@ export default class ActorTable extends Vue {
     { id: 1, first_name: "Dickerson", last_name: "Macdonald" },
     { id: 2, first_name: "Reina", last_name: "Rizka" },
   ];
+  private showAddModal = false;
+  private showUpdateModal = false;
+  private updateIndex = 0;
+  private lastId = 3;
 
   public deleteActor(index: number): void {
     this.actors.splice(index, 1);
+  }
+
+  public toggleModal(name: string): void {
+    if (name === "add") {
+      this.showAddModal = !this.showAddModal;
+    } else if (name === "update") {
+      this.showUpdateModal = !this.showUpdateModal;
+    }
+  }
+
+  public openUpdateModal(index: number) {
+    this.updateIndex = index;
+
+    this.toggleModal("update");
+  }
+
+  public addActor(firstName: string, lastName: string) {
+    this.actors.push({
+      id: this.lastId,
+      first_name: firstName,
+      last_name: lastName,
+    });
+
+    this.lastId++;
+  }
+
+  public updateActor(firstName: string, lastName: string) {
+    const newData = {
+      id: this.actors[this.updateIndex].id,
+      first_name: firstName,
+      last_name: lastName,
+    };
+
+    this.actors.splice(this.updateIndex, 1, newData);
+  }
+
+  get currentActor(): any {
+    return this.actors[this.updateIndex];
   }
 }
 </script>
